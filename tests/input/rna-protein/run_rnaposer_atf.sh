bf89163e5c9440914ba08c1cc5fb443f72e0ea99
Aaron T. Frank <afrankz@umich.edu>
2019-07-09 14:42:35 -0400

    updated validation script to make it more user friendly

--- tests/input/rna-protein/run_rnaposer_atf.sh
+++ tests/input/rna-protein/run_rnaposer_atf.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# RNAPOSER
+# setup environment
 module load gcc
 module load anaconda
+
+# intitialize 
 base=/home/afrankz/local_software/repo/RNAPoser/tests/input/rna-protein/ten_3drpc/
-rnaposer=/home/itssahil/PROJECTS/Mol_Feturizer/Test_systems_manuscript/RNAPoser-master/bin/rna_poser
-pdbs="1B7F"
+rnaposer=/home/afrankz/local_software/repo/RNAPoser
+cd ${rnaposer}
+
+# loop over test cases
+pdbs="1B7F 1DFU 1JBS 1P6V 1WPU 1WSU 2ASB 2BH2 2QUX 3BX2"
 for pdb in $pdbs
 do
-    ${rnaposer} ${base}/${pdb}_complex.pdb -trj ${base}/${pdb}_complex.dcd -mol2 ${base}/${pdb}_protein_from_pdb.mol2 -mode R -outfile ${base}/${pdb}_classifications.txt
-done    
-
-
-# Python
-./src/rna_poser_validation.sh ${base}/ ${pdb} ${pdb}_complex.pdb ${pdb}_protein_from_pdb.mol2 ${pdb}_complex.dcd
+    ./src/rna_poser_validation.sh ${base}/ ${pdb} ${pdb}_complex.pdb ${pdb}_protein_from_pdb.mol2 ${pdb}_complex.dcd 100 2.5 248 ${base}/${pdb}_classifications.txt
+done
